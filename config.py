61cf3d6202f68f38d0fde5e1b85fd6a133e30bab
Gabrielys21 <achavarriay@miumg.edu.gt>
2025-06-03 23:34:50 -0600

    update changes four

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # Archivo de configuración para almacenar claves de API y otros ajustes
 
-CHATGPT_API_KEY = 'sFJlk74dSOEoL80Q64L4zenxAkoA'
+CHATGPT_API_KEY = 'sk-proj--2JGL-_xlB21pXgTCpO7qFJ1Xy80Q64L4zenxAkoA'
 PHONE_NUMBER_ID = ''
-WHATSAPP_API_URL = f'https://graph.facebook.com/v19.0/{PHONE_NUMBER_ID}/messages'
-WHATSAPP_ACCESS_TOKEN = 'EugZD'
-ODOO_API_URL = '1'
+WHATSAPP_API_URL = f'hssages'
+WHATSAPP_ACCESS_TOKEN = ''
+ODOO_API_URL = 'https://ds=1'
 ODOO_API_KEY = ''
 
 # Nuevas variables de configuración
-VERIFY_TOKEN = ''
+VERIFY_TOKEN = 'Wa_Boyt_Secret_7z8PxK2yT9m'
